0473f275fea1c590af69c8898cf74509513fd58b
201-06 <student@201-06>
2022-10-28 16:33:21 +0300

    new features

--- main.py
+++ main.py
@@ -4,22 +4,25 @@ from telegram.ext import CommandHandler
 TOKEN = "5495345535:AAE6uAEJoYJbYFOOLi4LgYvmERjubhwxlEA"
 def echo(update, conext):
     txt = update.message.text
+    n = txt
     if txt.lower() in ['коооляяя', 'коля']:
         txt = "фто???)))"
-    if txt.lower() in ['любишь чокопай?', 'любишь чокопай']:
+    if txt.lower() in ['любишь чокопай?', 'любишь чокопай', 'хочешь чокопай?', 'хочешь чокопай']:
         txt = "да"
     if txt.lower() in ['с говном?', 'с говном']:
         txt = "на колени!!!"
-    if txt.lower() in ['че смотришь', 'чё смотришь']:
+    if txt.lower() in ['че смотришь', 'чё смотришь', 'че палишь?', 'чё палишь?', 'че палишь', 'чё палишь', 'че смотришь?', 'чё смотришь?']:
         txt = "хочу и смотрю!!!"
     if txt.lower() in ['ты кому', 'ты кому?']:
         txt = "тебе!!!"
-    if txt.lower() in ['сосешь', 'сосёшь']:
+    if txt.lower() in ['сосал', 'сосёшь', 'сосешь']:
         txt = ")))"
+    if txt == n:
+        txt = "хммм"
     update.message.reply_text(txt)
 
 def start(update, conext):
-    update.message.reply_text("Хочу и говарю")
+    update.message.reply_text("фто?")
 
 
 def main():
