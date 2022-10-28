e3e5028b61990e2d5c4a9c103c6785dc4c08baa9
201-06 <student@201-06>
2022-10-28 14:43:41 +0300

    kooooolya

---
+++ main.py
@@ -0,0 +1,29 @@
+from telegram.ext import Updater, MessageHandler, Filters
+from telegram.ext import CommandHandler
+
+TOKEN = "5495345535:AAE6uAEJoYJbYFOOLi4LgYvmERjubhwxlEA"
+def echo(update, conext):
+    txt = update.message.text
+    if txt.lower() in ['коооляяя', 'коля']:
+        txt = "фто???)))"
+    if txt.lower() in ['любишь чокопай?', 'любишь чокопай']:
+        txt = "да"
+    if txt.lower() in ['с говном?', 'с говном']:
+        txt = "на колени!!!"
+    update.message.reply_text(txt)
+
+def start(update, conext):
+    update.message.reply_text("Хочу и говарю")
+
+def main():
+    updater = Updater(TOKEN,use_context=True)
+    dp = updater.dispatcher
+    print("а мы стартуем")
+
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(MessageHandler(Filters.text, echo))
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
